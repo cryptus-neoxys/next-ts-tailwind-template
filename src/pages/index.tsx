d86075e6f783b38a33e0bd5f12ca65d3cd2e5382
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-04-07 22:00:22 +0530

    css cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,29 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
+        <h1 className="text-5xl font-bold">
+          Welcome to{" "}
+          <a className="text-blue-600" href="https://nextjs.org">
+            Next.js with{" "}
+            <a href="https://fauna.com/" className="text-green-400">
+              Tailwind
+            </a>{" "}
+            &
+            <a href="" className="text-blue-500">
+              {" "}
+              TypeScript
+            </a>
+          </a>
         </h1>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer">
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   );
 }
