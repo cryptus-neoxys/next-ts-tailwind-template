092a3aa76ad64efe0052bb5598d9c03caabb220d
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-04-07 21:31:58 +0530

    first commit

---
+++ src/pages/register.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="text-center">
+        <h1 className="text-2xl">
+          Welcome to <a href="https://nextjs.org">Register page</a>
+        </h1>
+      </main>
+    </div>
+  );
+}
